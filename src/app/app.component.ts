aefa4149e833e9812c8fa055f65f2e30970f695b
lucarauj <lukas.martins193@gmail.com>
2024-01-05 23:47:37 -0300

    feat: reestruturação do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
-import {InicioComponent} from './inicio/inicio.component'
-import {LoginComponent} from './login/login.component'
-import {MenuComponent} from './menu/menu.component'
-import {RodapeComponent} from './rodape/rodape.component'
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    CommonModule, 
-    RouterOutlet, 
-    InicioComponent,
-    LoginComponent,
-    MenuComponent,
-    RodapeComponent
-  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LK Games';
+  title = 'atividade-angular';
 }
